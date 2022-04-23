835416199f44564363c1542b5d538e74b2bae89a
Mathius <mathiuskormasela12@gmail.com>
2022-04-23 10:34:27 +0700

    I added the splash screen

--- src/StackScreen.tsx
+++ src/StackScreen.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RNBootSplash from 'react-native-bootsplash';
 
 // import all screens
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -10,17 +11,17 @@ import WelcomeScreen from './screens/WelcomeScreen';
 const Stack = createNativeStackNavigator();
 
 const StackScreen: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer onReady={() => RNBootSplash.hide()}>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="WelcomeScreen"
+					component={WelcomeScreen}
+					options={{headerShown: false}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default StackScreen;
